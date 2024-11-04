c840b731bc101492d1a9a8ac6d2462d753bbecbd
Diogo Pinheiro <diogo.pinheiro1996@gmail.com>
2024-11-04 16:30:50 +0000

    more cube

--- cub3d/cub3d.h
+++ cub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: diogosan <diogosan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:56:41 by diogosan          #+#    #+#             */
-/*   Updated: 2024/11/04 15:50:27 by diogosan         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:25:42 by diogosan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,51 @@
 # include "libraries/libft/libft.h"
 # include "libraries/printf/ft_printf.h"
 # include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+# include "minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <math.h>
+
+# define WIDTH 1920
+# define HEIGHT 1080
+
+typedef struct s_point
+{
+	float				x;
+	float				y;
+	int					z;
+	int					color;
+}	t_point;
+
+typedef struct s_map
+{
+	int					width;
+	int					height;
+	t_point				**coord;
+}	t_map;
+
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
+
+typedef struct s_mlx
+{
+	void	*mlx_connect;
+	void	*mlx_win;
+	t_map	*map;
+	int		zoom;
+	float	z_zoom;
+	int		shift_x;
+	int		shift_y;
+	float	view;
+	float	c_factor;
+	t_img	img;
+}	t_mlx;
 
 int	main(void);
 
--- cub3d/main.c
+++ cub3d/main.c
@@ -6,16 +6,35 @@
 /*   By: diogosan <diogosan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:56:01 by diogosan          #+#    #+#             */
-/*   Updated: 2024/11/04 16:11:30 by diogosan         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:30:02 by diogosan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-#include <time.h>
+
+void	ft_cleanup_and_exit(t_mlx *mlx)
+{
+	if (mlx->mlx_connect != NULL)
+	{
+		if (mlx->mlx_win != NULL)
+			mlx_destroy_window(mlx->mlx_connect, mlx->mlx_win);
+		mlx_destroy_display(mlx->mlx_connect);
+		free(mlx->mlx_connect);
+	}
+	free(mlx);
+	exit(0);
+}
+
+int	ft_close(t_mlx *mlx)
+{
+	ft_cleanup_and_exit(mlx);
+	return (0);
+}
 
 void	render(int map)
 {
 	char	*line;
+	t_mlx	*win;
 
 	line = get_next_line(map);
 	while (line != NULL)
@@ -24,6 +43,11 @@ void	render(int map)
 		free(line);
 		line = get_next_line(map);
 	}
+	win = malloc(sizeof(t_mlx));
+	win->mlx_connect = mlx_init();
+	win->mlx_win = mlx_new_window(win->mlx_connect, WIDTH, HEIGHT, "Cub");
+	mlx_hook(win->mlx_win, 17, 0, ft_close, win); // Set up a hook for closing events using ft_close function
+	mlx_loop(win->mlx_connect);
 }
 
 int	main(void)
